bbcec80ad370decfeeba5a5509749de052ab6d94
Pritish4113 <96116121+Pritish4113@users.noreply.github.com>
2023-09-16 18:02:29 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ const path = require("path");
 //middlewares
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "localhost:5000"
+}
+))
+app.options('*', cors())
 main(); //connecting to mongodb
 
 
