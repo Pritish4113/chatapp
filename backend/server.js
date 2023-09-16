72d3100f9458ae104636bfd942a43e4cd8ae88ef
Pritish4113 <96116121+Pritish4113@users.noreply.github.com>
2023-09-16 18:09:32 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-  origin: "localhost:5000"
+  origin: "https://chatbase-rcs5.onrender.com"
 }
 ))
 app.options('*', cors())
